d1ccf7c8c2ed4bc0e4af637a9b36e87fccb9c77f
cece hedrick <cecehedrick@gmail.com>
2022-05-03 20:50:31 -0500

    adds files to project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import styles from './styles.css';
+import {App} from './App';
+import React from 'react';
+ReactDOM.render(
+    <App />, 
+    document.getElementById('app')
+  );
